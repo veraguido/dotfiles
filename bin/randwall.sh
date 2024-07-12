2da6aef40e3b8eb97790e23e5c9a816c299eb94a
Guido Vera <vera.a.guido@gmail.com>
2024-07-12 09:17:51 +0100

    adding missing scripts

--- bin/randwall.sh
+++ bin/randwall.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+echo $DISPLAY
+export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
 wallpaper_path="/home/guido/MEGA/MEGAsync/wallpapers"
-swww img `find $wallpaper_path -type f | shuf -n 1` --transition-type any --transition-fps 60 --transition-pos 1,1
+
+if [ -z "$1" ]; then
+	swww img $(find $wallpaper_path -type f | shuf -n 1)
+	exit 1
+fi
+
+if [ $1 = "random" ]; then
+	swww img $(find $wallpaper_path -type f | shuf -n 1) --transition-type any --transition-fps 60 --transition-pos 1,1
+fi
